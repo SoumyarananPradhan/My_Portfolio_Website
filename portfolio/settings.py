f48d603bdb720a5332b51f99eb8a7462723a5951
Soumyaranjan <soumyaranjanaman54@gmail.com>
2026-02-01 22:46:18 +0530

    Edited Settings for PythonAnywhere

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 
 # REQUIRED for production (PythonAnywhere)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
