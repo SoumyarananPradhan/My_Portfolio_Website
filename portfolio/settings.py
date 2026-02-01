9a884311efcc7e873b2512a0626b606e2887f2a0
Soumyaranjan <soumyaranjanaman54@gmail.com>
2026-02-01 20:27:29 +0530

    Edited Settings for PythonAnywhere

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,11 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static/base')
-]
+STATIC_URL = '/static/'
+
+# REQUIRED for production (PythonAnywhere)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
